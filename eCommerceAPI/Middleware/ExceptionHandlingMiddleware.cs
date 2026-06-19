f3814893360bfdf38083c5b6e42e2828a33cb9e2
Elect-Trick <snmajenje@gmail.com>
2026-06-19 21:04:54 +0200

    Exception Handling
    Add Exception Handling middleware

---
+++ eCommerceAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace eCommerceAPI.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            try
+            {
+                await _next(httpContext);
+
+            }
+            catch (Exception e)
+            {
+                //Log the exception using the logger. 
+                _logger.LogError(e.Message);
+
+                if (e.InnerException is not null)
+                {
+                    _logger.LogError(e.InnerException.Message);
+                }
+                //Internal Server Error
+                httpContext.Response.StatusCode = 500;
+                await httpContext.Response.WriteAsJsonAsync(new
+                {
+                    Message = e.Message
+                });
+            }
+                
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}

0dafcdb79254d4ef653d79f9ff4a8c1624dd5fc5
Elect-Trick <snmajenje@gmail.com>
2026-06-20 21:37:42 +0200

    Dead code
    Remove dead using statements

--- eCommerceInfrastructure/DependencyInjection/DependencyInjection.cs
+++ eCommerceInfrastructure/DependencyInjection/DependencyInjection.cs
@@ -2,11 +2,7 @@
 using eCommerceInfrastructure.DbContext;
 using eCommerceInfrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace eCommerceInfrastructure.DependencyInjection;
    

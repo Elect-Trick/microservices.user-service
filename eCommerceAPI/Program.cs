884072bc15236925772b59860153ad3432b0860c
Elect-Trick <snmajenje@gmail.com>
2026-06-20 20:45:42 +0200

    Assemble Validators
    Add Validators to assembly containing

--- eCommerceAPI/Program.cs
+++ eCommerceAPI/Program.cs
@@ -2,6 +2,8 @@ using eCommerceAPI.Middleware;
 using eCommerceInfrastructure.DependencyInjection;
 using eCommerceCore.DependencyInjection;
 using eCommerceCore.Mapper;
+using FluentValidation;
+using eCommerceCore.Validator;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,8 @@ builder.Services.AddCoreLayer();
 builder.Services.AddAutoMapper(cfg => {
 cfg.AddMaps(typeof(ApplicationUserMappingProfile).Assembly);
     });
+
+builder.Services.AddValidatorsFromAssemblyContaining<LoginValidator>();
 
 //Add Controllers
 builder.Services.AddControllers();

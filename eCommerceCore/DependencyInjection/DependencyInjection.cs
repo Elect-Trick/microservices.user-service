0b78eccbe84c6adb10666e186bf3af65cff5969a
Elect-Trick <snmajenje@gmail.com>
2026-06-20 20:45:01 +0200

    DI IValidator
    Dependency Inject the IValidator in the core layer.

--- eCommerceCore/DependencyInjection/DependencyInjection.cs
+++ eCommerceCore/DependencyInjection/DependencyInjection.cs
@@ -1,7 +1,9 @@
 ﻿using eCommerceCore.ServiceContracts;
 using eCommerceCore.Services;
+using eCommerceCore.Validator;
 using Microsoft.Extensions.DependencyInjection;
-
+using FluentValidation;
+using eCommerceCore.DTO;
 
 namespace eCommerceCore.DependencyInjection;
 
@@ -12,6 +14,8 @@ public static class DependencyInjection
      //TODO
 
         services.AddTransient<IUserService, UserService>();
+        services.AddScoped<IValidator<LoginDTO>, LoginValidator>();
+
         return services;
     }
 }
